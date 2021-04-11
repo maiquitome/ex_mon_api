13315f321a7bd76c929726c027e777deacacef16
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-04-11 15:26:42 -0300

    :sparkles: creating the delete route

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -4,4 +4,8 @@ defmodule ExMon do
   defdelegate create_trainer(params),
     to: ExMon.Trainer.Create,
     as: :call
+
+  defdelegate delete_trainer(params),
+    to: ExMon.Trainer.Delete,
+    as: :call
 end
