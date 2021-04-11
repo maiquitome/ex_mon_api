6ed4e9c76708d61332b7315fb7658b60f20f3679
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-04-11 06:15:15 -0300

    :sparkles: handling errors using the Fallback Controller

---
+++ lib/ex_mon_web/controllers/fallback_controller.ex
@@ -0,0 +1,20 @@
+defmodule ExMonWeb.FallbackController do
+  use ExMonWeb, :controller
+
+  # every controller action receives first the connection
+  # and second the parameters that the action receives
+
+  # every fallback controller defines a call function that will
+  # be responsible for receiving any error that is being pushed forward
+  def call(conn, {:error, result}) do
+    conn
+    # Plug.Conn.put_status(conn, status)
+    |> put_status(:bad_request)
+    # When we want to render a view that
+    # doesn't have the same name as the controller we use put_view
+    # Phoenix.Controller.put_view(conn, module) Stores the view for rendering.
+    |> put_view(ExMonWeb.ErrorView)
+    # Phoenix.Controller.render(conn, template, assigns)
+    |> render("error.json", result: result)
+  end
+end
