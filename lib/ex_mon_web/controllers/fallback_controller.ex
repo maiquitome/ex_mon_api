9ad38dd4247eb1f61894bb07aaca993237e7d2e4
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-04-30 20:56:46 -0300

    :sparkles: handle with error with status 404

--- lib/ex_mon_web/controllers/fallback_controller.ex
+++ lib/ex_mon_web/controllers/fallback_controller.ex
@@ -6,6 +6,19 @@ defmodule ExMonWeb.FallbackController do
 
   # every fallback controller defines a call function that will
   # be responsible for receiving any error that is being pushed forward
+
+  def call(conn, {:error, %{message: error_message, status: status}}) do
+    conn
+    # Plug.Conn.put_status(conn, status)
+    |> put_status(status)
+    # When we want to render a view that
+    # doesn't have the same name as the controller we use put_view
+    # Phoenix.Controller.put_view(conn, module) Stores the view for rendering.
+    |> put_view(ExMonWeb.ErrorView)
+    # Phoenix.Controller.render(conn, template, assigns)
+    |> render("error.json", result: error_message)
+  end
+
   def call(conn, {:error, result}) do
     conn
     # Plug.Conn.put_status(conn, status)
