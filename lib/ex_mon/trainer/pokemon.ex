f6e32b69d641d2bd2bcf6292d0af7b108762e7f1
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-05-02 20:47:04 -0300

    :sparkles: checks if the trainer exists

--- lib/ex_mon/trainer/pokemon.ex
+++ lib/ex_mon/trainer/pokemon.ex
@@ -2,6 +2,9 @@ defmodule ExMon.Trainer.Pokemon do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias ExMon.Repo
+  alias ExMon.Trainer
+
   @primary_key {:id, Ecto.UUID, autogenerate: true}
   @foreign_key_type Ecto.UUID
 
@@ -32,14 +35,62 @@ defmodule ExMon.Trainer.Pokemon do
     :trainer_id
   ]
 
-  @spec build(map) :: {:error, Ecto.Changeset.t()} | {:ok, map}
+  @spec build(map) :: {:error, map} | {:ok, map}
+  @doc """
+  Validates the pokemon information to be sent to the database.
+
+  ## Examples
+
+      iex> params = %{name: "pikachu", nickname: "oi", weight: 50, types: ["electric"], trainer_id: "34f55bd0-179e-4442-8c9d-6d820e74a8d1"}
+      %{
+        name: "pikachu",
+        nickname: "oi",
+        trainer_id: "34f55bd0-179e-4442-8c9d-6d820e74a8d1",
+        types: ["electric"],
+        weight: 50
+      }
+
+      iex> ExMon.Trainer.Pokemon.build(params)
+      {:ok,
+      %ExMon.Trainer.Pokemon{
+        __meta__: #Ecto.Schema.Metadata<:built, "pokemons">,
+        id: nil,
+        inserted_at: nil,
+        name: "pikachu",
+        nickname: "oi",
+        trainer: #Ecto.Association.NotLoaded<association :trainer is not loaded>,
+        trainer_id: "34f55bd0-179e-4442-8c9d-6d820e74a8d1",
+        types: ["electric"],
+        updated_at: nil,
+        weight: 50
+      }}
+  """
 
   def build(map_with_changes) do
     map_with_changes
     |> changeset()
     # emulates operations such as Repo.insert
     |> apply_action(:insert)
+    |> handle_errors()
   end
+
+  defp handle_errors({:error, params}), do: {:error, params}
+
+  defp handle_errors({:ok, %{trainer_id: trainer_id} = params}) do
+    Repo.get(Trainer, trainer_id)
+    |> handle_trainer(params)
+
+    # if Repo.get(Trainer, trainer_id) do
+    #   {:ok, params}
+    # else
+    #   {:error, %{message: "trainer does not exist"}}
+    # end
+  end
+
+  defp handle_trainer({:ok, _schema}, params), do: {:ok, params}
+
+  defp handle_trainer({:error, _changeset}, _params),
+    do: {:error, %{message: "trainer does not exist"}}
 
   def changeset(map_with_changes) do
     %__MODULE__{}
